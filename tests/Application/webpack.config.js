926c14b1bb411b3a1bc4cc62fec0a6d5c48197dd
PiotrSzymanski2000 <PiotrekSz-18@wp.pl>
2025-03-11 14:49:43 +0100

    [Maintenance] fix webpack config

--- tests/Application/webpack.config.js
+++ tests/Application/webpack.config.js
@@ -1,61 +1,54 @@
 const path = require('path');
 const Encore = require('@symfony/webpack-encore');
-// const pluginConfig = require('../../webpack.config');
-const syliusBundles = path.resolve(
-    __dirname,
-    '../../vendor/sylius/sylius/src/Sylius/Bundle/',
-);
-const uiBundleScripts = path.resolve(
-    syliusBundles,
-    'UiBundle/Resources/private/js/',
-);
-const uiBundleResources = path.resolve(
-    syliusBundles,
-    'UiBundle/Resources/private/',
-);
 
-// Mollie Shop config
-Encore.setOutputPath('public/build/mollie-shop/')
-    .setPublicPath('/build/mollie-shop')
-    .addEntry('shop-entry', './assets/shop/entry.js')
-    .addEntry('mollie-shop-entry', '../../src/Resources/assets/shop/entry.js')
+const syliusBundles = path.resolve(__dirname, '../../vendor/sylius/sylius/src/Sylius/Bundle/');
+const uiBundleScripts = path.resolve(syliusBundles, 'UiBundle/Resources/private/js/');
+const uiBundleResources = path.resolve(syliusBundles, 'UiBundle/Resources/private/');
+
+const [mollieShop, mollieAdmin] = require('../../webpack.config.js')
+
+// Shop config
+Encore
+    .setOutputPath('public/build/shop/')
+    .setPublicPath('/build/shop')
+    .addEntry('shop-entry', '../../vendor/sylius/sylius/src/Sylius/Bundle/ShopBundle/Resources/private/entry.js')
     .disableSingleRuntimeChunk()
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
     .enableVersioning(Encore.isProduction())
     .enableSassLoader();
+
 const shopConfig = Encore.getWebpackConfig();
 
 shopConfig.resolve.alias['sylius/ui'] = uiBundleScripts;
 shopConfig.resolve.alias['sylius/ui-resources'] = uiBundleResources;
 shopConfig.resolve.alias['sylius/bundle'] = syliusBundles;
+shopConfig.resolve.alias['chart.js/dist/Chart.min'] = path.resolve(__dirname, 'node_modules/chart.js/dist/chart.min.js');
 shopConfig.name = 'shop';
 
 Encore.reset();
 
-// Mollie Admin config
-Encore.setOutputPath('public/build/mollie-admin/')
-    .setPublicPath('/build/mollie-admin')
-    .addEntry('admin-entry', './assets/admin/entry.js')
-    .addEntry('mollie-admin-entry', '../../src/Resources/assets/admin/entry.js')
+// Admin config
+Encore
+    .setOutputPath('public/build/admin/')
+    .setPublicPath('/build/admin')
+    .addEntry('admin-entry', '../../vendor/sylius/sylius/src/Sylius/Bundle/AdminBundle/Resources/private/entry.js')
     .disableSingleRuntimeChunk()
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
     .enableVersioning(Encore.isProduction())
     .enableSassLoader();
+
 const adminConfig = Encore.getWebpackConfig();
 
 adminConfig.resolve.alias['sylius/ui'] = uiBundleScripts;
 adminConfig.resolve.alias['sylius/ui-resources'] = uiBundleResources;
 adminConfig.resolve.alias['sylius/bundle'] = syliusBundles;
+adminConfig.resolve.alias['chart.js/dist/Chart.min'] = path.resolve(__dirname, 'node_modules/chart.js/dist/chart.min.js');
 adminConfig.externals = Object.assign({}, adminConfig.externals, {
     window: 'window',
     document: 'document',
 });
 adminConfig.name = 'admin';
 
-adminConfig.resolve.alias['chart.js/dist/Chart.min'] = 'chart.js/dist/chart.min'
-
-Encore.reset();
-
-module.exports = [shopConfig, adminConfig];
+module.exports = [shopConfig, adminConfig, mollieShop, mollieAdmin];
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,41 @@
 const path = require('path');
 const Encore = require('@symfony/webpack-encore');
+const pluginName = 'mollie';
 
-if (!Encore.isRuntimeEnvironmentConfigured()) {
-	Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+const getConfig = (pluginName, type) => {
+	Encore.reset();
+
+	Encore
+		.setOutputPath(`public/build/${pluginName}/${type}/`)
+		.setPublicPath(`/build/${pluginName}/${type}/`)
+		.addEntry(`${pluginName}-${type}`, path.resolve(__dirname, `./src/Resources/assets/${type}/entry.js`))
+		.disableSingleRuntimeChunk()
+		.cleanupOutputBeforeBuild()
+		.enableSourceMaps(!Encore.isProduction())
+		.enableVersioning(Encore.isProduction())
+		.enableSassLoader();
+
+	const config = Encore.getWebpackConfig();
+	config.name = `${pluginName}-${type}`;
+
+	return config;
 }
 
-Encore.setOutputPath('public/mollie')
-	.setPublicPath('/mollie')
-
-	.addEntry(
-		'mollie/admin',
-		path.resolve(__dirname, './src/Resources/assets/admin/entry.js')
-	)
-	.addEntry(
-		'mollie/shop',
-		path.resolve(__dirname, './src/Resources/assets/shop/entry.js')
-	)
-
-	.disableSingleRuntimeChunk()
+Encore
+	.setOutputPath(`src/Resources/public/`)
+	.setPublicPath(`/public/`)
+	.addEntry(`${pluginName}-shop`, path.resolve(__dirname, `./src/Resources/assets/shop/entry.js`))
+	.addEntry(`${pluginName}-admin`, path.resolve(__dirname, `./src/Resources/assets/admin/entry.js`))
 	.cleanupOutputBeforeBuild()
-	.enableSourceMaps(!Encore.isProduction())
+	.disableSingleRuntimeChunk()
 	.enableSassLoader();
 
-const pluginConfig = Encore.getWebpackConfig();
+const distConfig = Encore.getWebpackConfig();
+distConfig.name = `plugin-dist`;
 
-module.exports = pluginConfig;
+Encore.reset();
+
+const shopConfig = getConfig(pluginName, 'shop')
+const adminConfig = getConfig(pluginName, 'admin')
+
+module.exports = [shopConfig, adminConfig, distConfig];
