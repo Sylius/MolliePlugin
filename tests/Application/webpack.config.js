8f24e3d1f8a9a69029d588a5b48bffb36280dbaf
PiotrSzymanski2000 <PiotrekSz-18@wp.pl>
2025-03-12 08:23:10 +0100

    [Maintenance] fix after cr

--- tests/Application/webpack.config.js
+++ tests/Application/webpack.config.js
@@ -4,14 +4,13 @@ const Encore = require('@symfony/webpack-encore');
 const syliusBundles = path.resolve(__dirname, '../../vendor/sylius/sylius/src/Sylius/Bundle/');
 const uiBundleScripts = path.resolve(syliusBundles, 'UiBundle/Resources/private/js/');
 const uiBundleResources = path.resolve(syliusBundles, 'UiBundle/Resources/private/');
-
-const [mollieShop, mollieAdmin] = require('../../webpack.config.js')
+const pluginName = 'mollie';
 
 // Shop config
 Encore
     .setOutputPath('public/build/shop/')
     .setPublicPath('/build/shop')
-    .addEntry('shop-entry', '../../vendor/sylius/sylius/src/Sylius/Bundle/ShopBundle/Resources/private/entry.js')
+    .addEntry('shop-entry', './assets/shop/entry.js')
     .disableSingleRuntimeChunk()
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
@@ -19,7 +18,6 @@ Encore
     .enableSassLoader();
 
 const shopConfig = Encore.getWebpackConfig();
-
 shopConfig.resolve.alias['sylius/ui'] = uiBundleScripts;
 shopConfig.resolve.alias['sylius/ui-resources'] = uiBundleResources;
 shopConfig.resolve.alias['sylius/bundle'] = syliusBundles;
@@ -32,7 +30,7 @@ Encore.reset();
 Encore
     .setOutputPath('public/build/admin/')
     .setPublicPath('/build/admin')
-    .addEntry('admin-entry', '../../vendor/sylius/sylius/src/Sylius/Bundle/AdminBundle/Resources/private/entry.js')
+    .addEntry('admin-entry', './assets/admin/entry.js')
     .disableSingleRuntimeChunk()
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
@@ -40,15 +38,58 @@ Encore
     .enableSassLoader();
 
 const adminConfig = Encore.getWebpackConfig();
-
 adminConfig.resolve.alias['sylius/ui'] = uiBundleScripts;
 adminConfig.resolve.alias['sylius/ui-resources'] = uiBundleResources;
 adminConfig.resolve.alias['sylius/bundle'] = syliusBundles;
 adminConfig.resolve.alias['chart.js/dist/Chart.min'] = path.resolve(__dirname, 'node_modules/chart.js/dist/chart.min.js');
-adminConfig.externals = Object.assign({}, adminConfig.externals, {
-    window: 'window',
-    document: 'document',
-});
+adminConfig.externals = Object.assign({}, adminConfig.externals, { window: 'window', document: 'document' });
 adminConfig.name = 'admin';
 
-module.exports = [shopConfig, adminConfig, mollieShop, mollieAdmin];
+Encore.reset();
+
+// Mollie Shop config
+Encore
+    .setOutputPath(`public/build/${pluginName}/shop/`)
+    .setPublicPath(`/build/${pluginName}/shop/`)
+    .addEntry(`${pluginName}-shop`, path.resolve(__dirname, `../../src/Resources/assets/shop/entry.js`))
+    .disableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .enableSassLoader();
+
+const mollieShopConfig = Encore.getWebpackConfig();
+mollieShopConfig.name = `${pluginName}-shop`;
+
+Encore.reset();
+
+// Mollie Admin config
+Encore
+    .setOutputPath(`public/build/${pluginName}/admin/`)
+    .setPublicPath(`/build/${pluginName}/admin/`)
+    .addEntry(`${pluginName}-admin`, path.resolve(__dirname, `../../src/Resources/assets/admin/entry.js`))
+    .disableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .enableSassLoader();
+
+const mollieAdminConfig = Encore.getWebpackConfig();
+mollieAdminConfig.name = `${pluginName}-admin`;
+
+Encore.reset();
+
+// Mollie Dist config
+Encore
+    .setOutputPath(`src/Resources/public/`)
+    .setPublicPath(`/public/`)
+    .addEntry(`${pluginName}-shop`, path.resolve(__dirname, `../../src/Resources/assets/shop/entry.js`))
+    .addEntry(`${pluginName}-admin`, path.resolve(__dirname, `../../src/Resources/assets/admin/entry.js`))
+    .cleanupOutputBeforeBuild()
+    .disableSingleRuntimeChunk()
+    .enableSassLoader();
+
+const distConfig = Encore.getWebpackConfig();
+distConfig.name = `plugin-dist`;
+
+module.exports = [shopConfig, adminConfig, mollieShopConfig, mollieAdminConfig, distConfig];
