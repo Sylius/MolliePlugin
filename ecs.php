cbe4da5f68215a15ff0fd642c46e98048d063d50
Hubert Filar <hubert.filar@gmail.com>
2025-03-13 14:07:47 +0100

    [Maintenance] Adjust package name, update license and inject it to files

--- ecs.php
+++ ecs.php
@@ -1,29 +1,41 @@
 <?php
 
+/*
+ * This file is part of the Sylius Mollie Plugin package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return function (ECSConfig $ecsConfig): void {
-    $ecsConfig->paths([
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
+return static function (ECSConfig $config): void {
+    $config->parallel();
+    $config->paths([
+        'spec',
+        'src',
+        'tests/Behat',
+        'tests/PHPUnit',
+        'tests/Application/src',
     ]);
-
-    // this way you add a single rule
-    $ecsConfig->rules([
-        NoUnusedImportsFixer::class,
-    ]);
-
-    // this way you can add sets - group of rules
-    $ecsConfig->sets([
-        // run and fix, one by one
-        // SetList::SPACES,
-        // SetList::ARRAY,
-        // SetList::DOCBLOCK,
-        // SetList::NAMESPACES,
-        // SetList::COMMENTS,
-        // SetList::PSR_12,
-    ]);
+    $config->ruleWithConfiguration(
+        HeaderCommentFixer::class,
+        [
+            'location' => 'after_open',
+            'comment_type' => HeaderCommentFixer::HEADER_COMMENT,
+            'header' => <<<TEXT
+This file is part of the Sylius Mollie Plugin package.
+ 
+(c) Sylius Sp. z o.o.
+ 
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+TEXT
+        ]
+    );
 };
