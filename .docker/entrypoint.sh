3efd704b93ea797397d6a5b49d984529eb8b84bc
Michał Pysiak <mpysiak@gmail.com>
2025-03-21 10:59:44 +0100

    Add wkhtml, mailhog

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -8,6 +8,7 @@ echo "🚀 start services..."
 service nginx start
 service php8.1-fpm start
 service mariadb start
+mailhog > /dev/null 2>&1 &
 
 echo "🚀 Running database migrations..."
 cd tests/Application
@@ -16,11 +17,15 @@ bin/console doctrine:schema:update -f
 bin/console sylius:fixtures:load -n
 bin/console assets:install
 
+echo "🚀 Updating MAILER_DSN to use MailHog..."
+sed -i 's|^MAILER_DSN=.*|MAILER_DSN=smtp://127.0.0.1:1025|' /var/www/tests/Application/.env
+sed -i 's|^MAILER_URL=.*|MAILER_URL=smtp://127.0.0.1:1025|' /var/www/tests/Application/.env
+
 echo "🚀 Installing and building frontend assets..."
 yarn && yarn build
 
 echo "🚀 Set proper permissions..."
 chown -R www-data:www-data /var/www
 
-echo "✅ Initialization complete..."
+echo -e "\n\e[1;32m🟢 Mollie plugin demo is ready and available at: http://127.0.0.1:8080\e[0m\n"
 tail -f /dev/null
