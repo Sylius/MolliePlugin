777a70058746442392768e289d5f7960a4f0c37c
Wojdylak <karolwojdyla37@gmail.com>
2025-03-26 14:01:34 +0100

    [Migration] Rename table constraints

--- src/Migrations/Version20250326100123.php
+++ src/Migrations/Version20250326100123.php
@@ -46,6 +46,37 @@ final class Version20250326100123 extends AbstractMigration
         $this->addSql('ALTER TABLE mollie_subscription_payments RENAME INDEX idx_4653ad099a1887dc TO IDX_DC1E0C489A1887DC');
         $this->addSql('ALTER TABLE mollie_subscription_payments RENAME INDEX idx_4653ad094c3a3bb TO IDX_DC1E0C484C3A3BB');
         $this->addSql('ALTER TABLE mollie_subscription_schedule RENAME INDEX idx_79b927c0d38231d4 TO IDX_E3F48681D38231D4');
+
+        $this->addSql('ALTER TABLE mollie_configuration DROP CONSTRAINT FK_23CC8504EB71DAB7');
+        $this->addSql('ALTER TABLE mollie_configuration ADD CONSTRAINT FK_EDD60BC5EB71DAB7 FOREIGN KEY (payment_surcharge_fee) REFERENCES mollie_configuration_surcharge_fee (id)');
+        $this->addSql('ALTER TABLE mollie_configuration DROP CONSTRAINT FK_CONFIG_AMOUNT_LIMITS');
+        $this->addSql('ALTER TABLE mollie_configuration ADD CONSTRAINT FK_EDD60BC52306388E FOREIGN KEY (amount_limits_id) REFERENCES mollie_configuration_amount_limits (id)');
+        $this->addSql('ALTER TABLE mollie_configuration DROP CONSTRAINT FK_23CC8504DBC26BFF');
+        $this->addSql('ALTER TABLE mollie_configuration ADD CONSTRAINT FK_EDD60BC5DBC26BFF FOREIGN KEY (method_image_id) REFERENCES mollie_method_image (id)');
+        $this->addSql('ALTER TABLE mollie_configuration DROP CONSTRAINT FK_23CC85048298F');
+        $this->addSql('ALTER TABLE mollie_configuration ADD CONSTRAINT FK_EDD60BC5C6B58E54 FOREIGN KEY (default_category_id) REFERENCES mollie_product_type (id)');
+        $this->addSql('ALTER TABLE mollie_configuration DROP CONSTRAINT FK_23CC8504577F8E00');
+        $this->addSql('ALTER TABLE mollie_configuration ADD CONSTRAINT FK_EDD60BC5577F8E00 FOREIGN KEY (gateway_id) REFERENCES sylius_gateway_config (id)');
+        $this->addSql('ALTER TABLE mollie_configuration_translation DROP CONSTRAINT FK_369B057A2C2AC5D3');
+        $this->addSql('ALTER TABLE mollie_configuration_translation ADD CONSTRAINT FK_988446672C2AC5D3 FOREIGN KEY (translatable_id) REFERENCES mollie_configuration (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE mollie_email_template_translation DROP CONSTRAINT FK_42127E4A2C2AC5D3');
+        $this->addSql('ALTER TABLE mollie_email_template_translation ADD CONSTRAINT FK_21BA0DD02C2AC5D3 FOREIGN KEY (translatable_id) REFERENCES mollie_email_template (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE mollie_subscription DROP CONSTRAINT FK_5E346303B3185C');
+        $this->addSql('ALTER TABLE mollie_subscription ADD CONSTRAINT FK_255E3D74B3185C FOREIGN KEY (subscription_configuration_id) REFERENCES mollie_subscription_configuration (id)');
+        $this->addSql('ALTER TABLE mollie_subscription DROP CONSTRAINT FK_5E3463039395C3F3');
+        $this->addSql('ALTER TABLE mollie_subscription ADD CONSTRAINT FK_255E3D749395C3F3 FOREIGN KEY (customer_id) REFERENCES sylius_customer (id)');
+        $this->addSql('ALTER TABLE mollie_subscription DROP CONSTRAINT FK_5E346303E415FB15');
+        $this->addSql('ALTER TABLE mollie_subscription ADD CONSTRAINT FK_255E3D74E415FB15 FOREIGN KEY (order_item_id) REFERENCES sylius_order_item (id)');
+        $this->addSql('ALTER TABLE mollie_subscription_orders DROP CONSTRAINT FK_DCE71BD39A1887DC');
+        $this->addSql('ALTER TABLE mollie_subscription_orders ADD CONSTRAINT FK_AC10D4809A1887DC FOREIGN KEY (subscription_id) REFERENCES mollie_subscription (id)');
+        $this->addSql('ALTER TABLE mollie_subscription_orders DROP CONSTRAINT FK_DCE71BD38D9F6D38');
+        $this->addSql('ALTER TABLE mollie_subscription_orders ADD CONSTRAINT FK_AC10D4808D9F6D38 FOREIGN KEY (order_id) REFERENCES sylius_order (id)');
+        $this->addSql('ALTER TABLE mollie_subscription_payments DROP CONSTRAINT FK_4653AD099A1887DC');
+        $this->addSql('ALTER TABLE mollie_subscription_payments ADD CONSTRAINT FK_DC1E0C489A1887DC FOREIGN KEY (subscription_id) REFERENCES mollie_subscription (id)');
+        $this->addSql('ALTER TABLE mollie_subscription_payments DROP CONSTRAINT FK_4653AD094C3A3BB');
+        $this->addSql('ALTER TABLE mollie_subscription_payments ADD CONSTRAINT FK_DC1E0C484C3A3BB FOREIGN KEY (payment_id) REFERENCES sylius_payment (id)');
+        $this->addSql('ALTER TABLE mollie_subscription_schedule DROP CONSTRAINT FK_79B927C0D38231D4');
+        $this->addSql('ALTER TABLE mollie_subscription_schedule ADD CONSTRAINT FK_E3F48681D38231D4 FOREIGN KEY (mollie_subscription_id) REFERENCES mollie_subscription (id)');
     }
 
     public function down(Schema $schema): void
@@ -71,5 +102,36 @@ final class Version20250326100123 extends AbstractMigration
         $this->addSql('ALTER TABLE mollie_subscription_payments RENAME INDEX idx_dc1e0c484c3a3bb TO IDX_4653AD094C3A3BB');
         $this->addSql('ALTER TABLE mollie_subscription_payments RENAME INDEX idx_dc1e0c489a1887dc TO IDX_4653AD099A1887DC');
         $this->addSql('ALTER TABLE mollie_subscription_schedule RENAME INDEX idx_e3f48681d38231d4 TO IDX_79B927C0D38231D4');
+
+        $this->addSql('ALTER TABLE mollie_configuration DROP CONSTRAINT FK_EDD60BC5EB71DAB7');
+        $this->addSql('ALTER TABLE mollie_configuration ADD CONSTRAINT FK_23CC8504EB71DAB7 FOREIGN KEY (payment_surcharge_fee) REFERENCES mollie_configuration_surcharge_fee (id)');
+        $this->addSql('ALTER TABLE mollie_configuration DROP CONSTRAINT FK_EDD60BC52306388E');
+        $this->addSql('ALTER TABLE mollie_configuration ADD CONSTRAINT FK_CONFIG_AMOUNT_LIMITS FOREIGN KEY (amount_limits_id) REFERENCES mollie_configuration_amount_limits (id)');
+        $this->addSql('ALTER TABLE mollie_configuration DROP CONSTRAINT FK_EDD60BC5DBC26BFF');
+        $this->addSql('ALTER TABLE mollie_configuration ADD CONSTRAINT FK_23CC8504DBC26BFF FOREIGN KEY (method_image_id) REFERENCES mollie_method_image (id)');
+        $this->addSql('ALTER TABLE mollie_configuration DROP CONSTRAINT FK_EDD60BC5C6B58E54');
+        $this->addSql('ALTER TABLE mollie_configuration ADD CONSTRAINT FK_23CC85048298F FOREIGN KEY (default_category_id) REFERENCES mollie_product_type (id)');
+        $this->addSql('ALTER TABLE mollie_configuration DROP CONSTRAINT FK_EDD60BC5577F8E00');
+        $this->addSql('ALTER TABLE mollie_configuration ADD CONSTRAINT FK_23CC8504577F8E00 FOREIGN KEY (gateway_id) REFERENCES sylius_gateway_config (id)');
+        $this->addSql('ALTER TABLE mollie_configuration_translation DROP CONSTRAINT FK_988446672C2AC5D3');
+        $this->addSql('ALTER TABLE mollie_configuration_translation ADD CONSTRAINT FK_369B057A2C2AC5D3 FOREIGN KEY (translatable_id) REFERENCES mollie_configuration (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE mollie_email_template_translation DROP CONSTRAINT FK_21BA0DD02C2AC5D3');
+        $this->addSql('ALTER TABLE mollie_email_template_translation ADD CONSTRAINT FK_42127E4A2C2AC5D3 FOREIGN KEY (translatable_id) REFERENCES mollie_email_template (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE mollie_subscription DROP CONSTRAINT FK_255E3D74B3185C');
+        $this->addSql('ALTER TABLE mollie_subscription ADD CONSTRAINT FK_5E346303B3185C FOREIGN KEY (subscription_configuration_id) REFERENCES mollie_subscription_configuration (id)');
+        $this->addSql('ALTER TABLE mollie_subscription DROP CONSTRAINT FK_255E3D749395C3F3');
+        $this->addSql('ALTER TABLE mollie_subscription ADD CONSTRAINT FK_5E3463039395C3F3 FOREIGN KEY (customer_id) REFERENCES sylius_customer (id)');
+        $this->addSql('ALTER TABLE mollie_subscription DROP CONSTRAINT FK_255E3D74E415FB15');
+        $this->addSql('ALTER TABLE mollie_subscription ADD CONSTRAINT FK_5E346303E415FB15 FOREIGN KEY (order_item_id) REFERENCES sylius_order_item (id)');
+        $this->addSql('ALTER TABLE mollie_subscription_orders DROP CONSTRAINT FK_AC10D4809A1887DC');
+        $this->addSql('ALTER TABLE mollie_subscription_orders ADD CONSTRAINT FK_DCE71BD39A1887DC FOREIGN KEY (subscription_id) REFERENCES mollie_subscription (id)');
+        $this->addSql('ALTER TABLE mollie_subscription_orders DROP CONSTRAINT FK_AC10D4808D9F6D38');
+        $this->addSql('ALTER TABLE mollie_subscription_orders ADD CONSTRAINT FK_DCE71BD38D9F6D38 FOREIGN KEY (order_id) REFERENCES sylius_order (id)');
+        $this->addSql('ALTER TABLE mollie_subscription_payments DROP CONSTRAINT FK_DC1E0C489A1887DC');
+        $this->addSql('ALTER TABLE mollie_subscription_payments ADD CONSTRAINT FK_4653AD099A1887DC FOREIGN KEY (subscription_id) REFERENCES mollie_subscription (id)');
+        $this->addSql('ALTER TABLE mollie_subscription_payments DROP CONSTRAINT FK_DC1E0C484C3A3BB');
+        $this->addSql('ALTER TABLE mollie_subscription_payments ADD CONSTRAINT FK_4653AD094C3A3BB FOREIGN KEY (payment_id) REFERENCES sylius_payment (id)');
+        $this->addSql('ALTER TABLE mollie_subscription_schedule DROP CONSTRAINT FK_E3F48681D38231D4');
+        $this->addSql('ALTER TABLE mollie_subscription_schedule ADD CONSTRAINT FK_79B927C0D38231D4 FOREIGN KEY (mollie_subscription_id) REFERENCES mollie_subscription (id)');
     }
 }
