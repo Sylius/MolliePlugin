4e8555e73a7d55cb611e816009e561adfd8bc9e9
Jan Goralski <jan.wojciech.goralski@gmail.com>
2025-05-09 16:47:12 +0200

    [Admin][PaymentMethod] Fix base form id in key testing JS

--- assets/admin/js/molliePayments/testApiKeys.js
+++ assets/admin/js/molliePayments/testApiKeys.js
@@ -1,12 +1,12 @@
 import $ from 'jquery';
 
 $(function () {
-    const testApiKeyButton = document.getElementsByClassName(' test-api-key-button');
+    const testApiKeyButton = document.getElementsByClassName('test-api-key-button');
 
     $(testApiKeyButton).on('click', function (event) {
         const testApiDataDiv = document.getElementsByClassName('test-api-key-div');
-        const testApiKey = document.getElementById('sylius_payment_method_gatewayConfig_config_api_key_test');
-        const liveApiKey = document.getElementById('sylius_payment_method_gatewayConfig_config_api_key_live');
+        const testApiKey = document.getElementById('sylius_admin_payment_method_gatewayConfig_config_api_key_test');
+        const liveApiKey = document.getElementById('sylius_admin_payment_method_gatewayConfig_config_api_key_live');
 
         $(this).addClass('loading');
         $(this).attr('disabled', true);
