2da6d458a22f8481bee07df65b9d9e68e8c70a52
PiotrSzymanski2000 <PiotrekSz-18@wp.pl>
2025-05-19 11:16:56 +0200

    [Maintenance] cr fixes

--- src/StateMachine/Applicator/MollieOrderStatesApplicator.php
+++ src/StateMachine/Applicator/MollieOrderStatesApplicator.php
@@ -34,29 +34,26 @@ final class MollieOrderStatesApplicator implements MollieOrderStatesApplicatorIn
         if (null === $order->orderNumber) {
             return;
         }
-        /** @var OrderInterface $orderSylius */
+
+        /** @var OrderInterface|null $orderSylius */
         $orderSylius = $this->orderRepository->findOneBy(['number' => $order->orderNumber]);
+        if (null === $orderSylius) {
+            return;
+        }
 
-        /** @var ShipmentInterface $firstShipment */
-        $firstShipment = $orderSylius->getShipments()->first();
-
-        /** @var ShipmentInterface $lastShipment */
+        /** @var ShipmentInterface|null $lastShipment */
         $lastShipment = $orderSylius->getShipments()->last();
+        if (null === $lastShipment) {
+            return;
+        }
 
         if ($order->isCompleted()) {
             $this->applyOrderTransition($orderSylius, OrderTransitions::TRANSITION_FULFILL);
-            $this->applyShipmentTransition($firstShipment, ShipmentTransitions::TRANSITION_SHIP);
+            $this->applyShipmentTransition($lastShipment, ShipmentTransitions::TRANSITION_SHIP);
         }
 
         if ($order->isCanceled() || $order->isExpired()) {
             $this->applyOrderTransition($orderSylius, OrderTransitions::TRANSITION_CANCEL);
-        }
-
-        if (
-            $order->isShipping() &&
-            $this->isConfirmNotify($order, $firstShipment)
-        ) {
-            return;
         }
 
         if ($order->isShipping()) {
@@ -73,29 +70,29 @@ final class MollieOrderStatesApplicator implements MollieOrderStatesApplicatorIn
         $this->stateMachine->apply($orderSylius, OrderTransitions::GRAPH, $transition);
     }
 
-    private function applyShipmentTransition(ShipmentInterface $orderSylius, string $transition): void
+    private function applyShipmentTransition(ShipmentInterface $shipment, string $transition): void
     {
-        if (!$this->stateMachine->can($orderSylius, ShipmentTransitions::GRAPH, $transition)) {
+        if (!$this->stateMachine->can($shipment, ShipmentTransitions::GRAPH, $transition)) {
             return;
         }
 
-        $this->stateMachine->apply($orderSylius, ShipmentTransitions::GRAPH, $transition);
+        $this->stateMachine->apply($shipment, ShipmentTransitions::GRAPH, $transition);
     }
 
     private function isConfirmNotify(Order $order, ShipmentInterface $shipment): bool
     {
-        // check if in mollie and sylius is the same shipped items
         $shippableQuantity = 0;
+
         foreach ($order->lines as $line) {
-            if (!property_exists($line, 'type')) {
-                throw new \InvalidArgumentException();
+            if (!property_exists($line, 'type') || $line->type !== 'physical') {
+                continue;
             }
-            if ('physical' === $line->type) {
-                if (!property_exists($line, 'shippableQuantity')) {
-                    throw new \InvalidArgumentException();
-                }
-                $shippableQuantity += $line->shippableQuantity;
+
+            if (!property_exists($line, 'shippableQuantity')) {
+                throw new \InvalidArgumentException('Missing shippableQuantity property.');
             }
+
+            $shippableQuantity += $line->shippableQuantity;
         }
 
         return $shippableQuantity === count($shipment->getUnits()->toArray());
