3e1d51f60071c87dea7bc88cb87be9f76d542db9
Wojdylak <karolwojdyla37@gmail.com>
2025-06-03 15:43:28 +0200

    Minor improvement

--- src/Processor/PaymentSurchargeProcessorInterface.php
+++ src/Processor/PaymentSurchargeProcessorInterface.php
@@ -19,9 +19,8 @@ use Sylius\MolliePlugin\Entity\OrderInterface;
 trigger_deprecation(
     'sylius/mollie-plugin',
     '2.2',
-    'The "%s" class is deprecated and will be removed in MolliePlugin 3.0. Use "%s" instead.',
+    'The "%s" class is deprecated and will be removed in MolliePlugin 3.0.',
     PaymentSurchargeProcessorInterface::class,
-    OrderProcessorInterface::class,
 );
 interface PaymentSurchargeProcessorInterface
 {
